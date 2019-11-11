ba76e866c338cbe0a492f5ec0e395b9295d11fbe
rohit <ol.chandra@gmail.com>
2019-11-11 08:44:45 +0100

    first trial rough code of no_entry_layer

---
+++ no_entry_layer/include/no_entry_layer/no_entry_layer_node.h
@@ -0,0 +1,10 @@
+// #ifndef NO_ENTRY_LAYER_NODE_H
+// #define NO_ENTRY_LAYER_NODE_H
+
+// #define _USE_MATH_DEFINES
+#include <ros/ros.h>
+#include <iostream>
+#include <math.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <no_entry_layer/carto_mask_configConfig.h>
+#include <dynamic_reconfigure/server.h>
